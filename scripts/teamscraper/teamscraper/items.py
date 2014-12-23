58fcd6d0738bfaaeae5fd5d430ee28d9fab12d1c
Vignesh Raja <vignesh@vigneshraja.com>
2014-12-23 02:45:52 -0800

    Added spider for scraping team information

---
+++ scripts/teamscraper/teamscraper/items.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class TeamscraperItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    rebound_ranges = scrapy.Field()
+    points_ranges = scrapy.Field()
---
+++ scripts/teamscraper/teamscraper/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for teamscraper project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'teamscraper'
+
+SPIDER_MODULES = ['teamscraper.spiders']
+NEWSPIDER_MODULE = 'teamscraper.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'teamscraper (+http://www.yourdomain.com)'
