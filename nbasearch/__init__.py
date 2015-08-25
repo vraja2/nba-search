d530db06ac6f1bd4ffddb237b93e55887cc4f118
Vignesh Raja <vignesh@vigneshraja.com>
2015-08-25 00:40:56 -0500

    Use years to distinguish between players with same name

--- nbasearch/__init__.py
+++ nbasearch/__init__.py
@@ -1,3 +1,4 @@
+import json
 import pickle
 import os
 from collections import defaultdict
@@ -8,6 +9,7 @@ app = Flask(__name__)
 def init_data():
   init_player_data()
   init_team_data()
+  init_player_list()
 
 def open_file(file_path):
   return open(os.path.join(os.path.dirname(__file__),file_path))
@@ -24,7 +26,11 @@ def init_player_data():
       break
     else:
       player_name = player['player_name'] 
-      player_dict[player_name].append(player['teams_played_on'])
+      teams_played_on = player['teams_played_on']
+      if teams_played_on:
+        first_year = teams_played_on[0][1]
+        last_year = teams_played_on[-1][1]
+        player_dict[(player_name,first_year,last_year)].append(player['teams_played_on'])
 
 #map from (team_name,year) to point/rebound ranges. rebound range is index 0, points range is 1
 team_dict = defaultdict(list)
@@ -40,5 +46,11 @@ def init_team_data():
       key_tup = (team['team'],team['year'])
       team_dict[key_tup].append(team['rebound_ranges'])
       team_dict[key_tup].append(team['points_ranges'])
+
+player_list = []
+
+def init_player_list():
+  for player_tuple,player_teams in player_dict.iteritems():
+      player_list.append('{} ({}-{})'.format(player_tuple[0],player_tuple[1],player_tuple[2]))
 
 import nbasearch.views
