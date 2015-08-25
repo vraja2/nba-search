9d424891a65b73af836730eb9e4d5a8aab1fb901
Vignesh Raja <vignesh@vigneshraja.com>
2015-08-25 00:43:03 -0500

    Initial front-end

--- nbasearch/views.py
+++ nbasearch/views.py
@@ -1,6 +1,13 @@
-from nbasearch import app, player_dict, team_dict
+from flask import render_template
+from nbasearch import app, player_dict, team_dict, player_list
+import json
 
 @app.route('/')
 def index():
-  #simple test to see if dictionaries produce expected results
-  return str(team_dict[player_dict['Michael Jordan'][0][0]])
+  return render_template("index.html", player_list=json.dumps(player_list))
+  #return str(team_dict[player_dict['Michael Jordan'][0][-1]])
+
+def get_teammates():
+  rebound_range = 2
+  points_range = 4
+
